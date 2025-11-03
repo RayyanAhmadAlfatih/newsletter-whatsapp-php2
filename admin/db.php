300ae259c716e01afb9c95b93054f078604ba01b
Rayyan Ahmad Alfatih <raybar.vada@gmail.com>
2025-11-03 10:26:47 +0700

    menganti code di file db.php

--- admin/db.php
+++ admin/db.php
@@ -5,10 +5,9 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 // Konfigurasi Database
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'newsletter_wa');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-
+define('DB_NAME', 'king_newsletter_wa');
+define('DB_USER', 'newsletter_wa');
+define('DB_PASS', 'newsletter_wa');
 // Koneksi Database menggunakan PDO
 try {
     $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8mb4", DB_USER, DB_PASS);
@@ -19,7 +18,7 @@ try {
 }
 
 // Konfigurasi Fonnte API
-define('FONNTE_API_KEY', 'YOUR_FONNTE_API_KEY'); // Ganti dengan API key Fonnte Anda
+define('FONNTE_API_KEY', 'q614EBFZ!GK@7-td!LdE'); // Ganti dengan API key Fonnte Anda
 define('FONNTE_API_URL', 'https://api.fonnte.com/send');
 
 // Konfigurasi Admin
